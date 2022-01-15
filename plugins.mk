82ef7678a6b2906a13c7bc9c9758d3ce55519b43
sadko4u <sadko4u@gmail.com>
2022-01-16 01:42:57 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-mb-gate.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_MB_GATE
-ARTIFACT_NAME               = lsp-plugins-mb-gate
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Multiband Gate Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_MB_GATE
+PLUGIN_PACKAGE_NAME     = lsp-plugins
