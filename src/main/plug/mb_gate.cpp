4b9f22535f230d1498fdc5d19147dc355e6f6e27
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-07-10 15:03:53 +0300

    Fixed improper phase compensation between Dry and Wet channels in Linear Phase mode.

--- src/main/plug/mb_gate.cpp
+++ src/main/plug/mb_gate.cpp
@@ -1119,7 +1119,7 @@ namespace lsp
                 c->sDelay.set_delay(latency);
                 c->sDryDelay.set_delay(latency + xover_latency);
                 sAnalyzer.set_channel_delay(c->nAnInChannel, xover_latency);
-                c->sXOverDelay.set_delay(latency + xover_latency);
+                c->sXOverDelay.set_delay(xover_latency);
             }
 
             nEnvBoost       = env_boost;
