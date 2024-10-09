63a98ff671e1bd710cb641cdce69d8fe98e4f8d4
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-10-09 21:06:54 +0300

    * Fixed compilation on modern compilers

--- src/main/plug/mb_gate.cpp
+++ src/main/plug/mb_gate.cpp
@@ -38,48 +38,51 @@ namespace lsp
     {
         //-------------------------------------------------------------------------
         // Plugin factory
-        typedef struct plugin_settings_t
+        inline namespace
         {
-            const meta::plugin_t   *metadata;
-            bool                    sc;
-            uint8_t                 mode;
-        } plugin_settings_t;
+            typedef struct plugin_settings_t
+            {
+                const meta::plugin_t   *metadata;
+                bool                    sc;
+                uint8_t                 mode;
+            } plugin_settings_t;
 
-        static const meta::plugin_t *plugins[] =
-        {
-            &meta::mb_gate_mono,
-            &meta::mb_gate_stereo,
-            &meta::mb_gate_lr,
-            &meta::mb_gate_ms,
-            &meta::sc_mb_gate_mono,
-            &meta::sc_mb_gate_stereo,
-            &meta::sc_mb_gate_lr,
-            &meta::sc_mb_gate_ms
-        };
+            static const meta::plugin_t *plugins[] =
+            {
+                &meta::mb_gate_mono,
+                &meta::mb_gate_stereo,
+                &meta::mb_gate_lr,
+                &meta::mb_gate_ms,
+                &meta::sc_mb_gate_mono,
+                &meta::sc_mb_gate_stereo,
+                &meta::sc_mb_gate_lr,
+                &meta::sc_mb_gate_ms
+            };
 
-        static const plugin_settings_t plugin_settings[] =
-        {
-            { &meta::mb_gate_mono,          false, mb_gate::MBGM_MONO         },
-            { &meta::mb_gate_stereo,        false, mb_gate::MBGM_STEREO       },
-            { &meta::mb_gate_lr,            false, mb_gate::MBGM_LR           },
-            { &meta::mb_gate_ms,            false, mb_gate::MBGM_MS           },
-            { &meta::sc_mb_gate_mono,       true,  mb_gate::MBGM_MONO         },
-            { &meta::sc_mb_gate_stereo,     true,  mb_gate::MBGM_STEREO       },
-            { &meta::sc_mb_gate_lr,         true,  mb_gate::MBGM_LR           },
-            { &meta::sc_mb_gate_ms,         true,  mb_gate::MBGM_MS           },
+            static const plugin_settings_t plugin_settings[] =
+            {
+                { &meta::mb_gate_mono,          false, mb_gate::MBGM_MONO         },
+                { &meta::mb_gate_stereo,        false, mb_gate::MBGM_STEREO       },
+                { &meta::mb_gate_lr,            false, mb_gate::MBGM_LR           },
+                { &meta::mb_gate_ms,            false, mb_gate::MBGM_MS           },
+                { &meta::sc_mb_gate_mono,       true,  mb_gate::MBGM_MONO         },
+                { &meta::sc_mb_gate_stereo,     true,  mb_gate::MBGM_STEREO       },
+                { &meta::sc_mb_gate_lr,         true,  mb_gate::MBGM_LR           },
+                { &meta::sc_mb_gate_ms,         true,  mb_gate::MBGM_MS           },
 
-            { NULL, 0, false }
-        };
+                { NULL, 0, false }
+            };
 
-        static plug::Module *plugin_factory(const meta::plugin_t *meta)
-        {
-            for (const plugin_settings_t *s = plugin_settings; s->metadata != NULL; ++s)
-                if (s->metadata == meta)
-                    return new mb_gate(s->metadata, s->sc, s->mode);
-            return NULL;
-        }
+            static plug::Module *plugin_factory(const meta::plugin_t *meta)
+            {
+                for (const plugin_settings_t *s = plugin_settings; s->metadata != NULL; ++s)
+                    if (s->metadata == meta)
+                        return new mb_gate(s->metadata, s->sc, s->mode);
+                return NULL;
+            }
 
-        static plug::Factory factory(plugin_factory, plugins, 8);
+            static plug::Factory factory(plugin_factory, plugins, 8);
+        } /* inline namespace */
 
         //-------------------------------------------------------------------------
         mb_gate::mb_gate(const meta::plugin_t *metadata, bool sc, size_t mode):
